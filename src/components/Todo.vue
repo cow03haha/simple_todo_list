0c953b6a2672d5af9642ed25373bc1e5d904ba1c
cow03haha <jaron111222@gmail.com>
2022-01-18 17:41:09 +0800

    add Todo compoents

---
+++ src/components/Todo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="todo">
+    <input type="text" v-model="text" @keyup.enter="addtask">
+    <button @click="addtask">Submit</button>
+    <hr>
+    <ol>
+        <li v-for="(task, index) in todo_list" :key="task">
+            {{task}} <button @click="cleartask(index)" class="remove">X</button>
+        </li>
+    </ol>
+    <hr>
+    <div v-if="todo_list.length">
+        Total: {{todo_list.length}} task
+        <button @click="clearall">Clear All</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Todo",
+    data(){
+        return{
+            todo_list: [],
+            text: ""
+        }
+    },
+    methods:{
+        addtask(){
+            this.todo_list.push(this.text)
+            this.text = ""
+        },
+        cleartask(index){
+            this.todo_list.splice(index, 1)
+        },
+        clearall(){
+            this.todo_list = []
+        }
+    }
+}
+</script>
+
+<style>
+    li {
+        padding: 5px;
+        text-align: left;
+        width: 20%;
+        margin: auto;
+    }
+</style>
